eaa79e35fc45ff94baabf54a7778c4be3c603c5a
Egor <tyrandel@MacBook-Air-Egor.local>
2023-09-13 19:49:50 +0300

    simple node example test

--- main.go
+++ main.go
@@ -3,33 +3,51 @@ package main
 import (
 	"PowBlockchain/blockchain"
 	"PowBlockchain/network"
-	"bufio"
 	"fmt"
-	"os"
-	"strconv"
-	"strings"
 )
 
 const DBNAME = "blockchain.db"
-const NODEADDR = "localhost:8080"
+const TransactionOption = 1
 
 func main() {
-	user := blockchain.LoadUser("MIIBOwIBAAJBAKNqVNRZgQ4nxf/3KbQkFTICWx74DJ0kjr/Juzu9Orx+Y/apKmided0Azsg6u2HolUN2P7YMe4rzqBnGWhdGWRECAwEAAQJAWQMoZerDA2Ti00RccQVejjj+TWYr6MTrBMjrteSjQ9xtuoik5cA85JMp3T+UOTokD+tQG3SNFETRi42aSDky4QIhANQcqBNbhywtICGiSxT7TRyoVMK+refwJSouApcMAtqVAiEAxTpEynk1F5/I77HSquJd0VBszPalyJDahs+HxKp74Y0CIQC//ZBEtTwMyGulBflf7HdH0TWncGCI59076Jl/juemYQIgdnlyKU52HiLVyWbAbfZc9Qei09y16a1aF/FCVVkz4WECIQDCov/RUtRytQ4C0MVmmLhjz8i7YzYbbiDWV1fPo7rPww==")
-	fmt.Println("Your privateKey:" + blockchain.StringPrivate(user.Private()))
-	fmt.Println("Your address:" + blockchain.StringPublic(user.Public()))
-	for {
-		fmt.Print("Enter tx: ")
-		msg, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-		splitedMsg := strings.Split(msg, " ")
-		toAddr := splitedMsg[0]
-		amount, _ := strconv.Atoi(splitedMsg[1])
-		chain := blockchain.LoadChain(DBNAME)
-
-		tx := blockchain.NewTransaction(user, chain.LastHash(), toAddr, uint64(amount))
-		packge := network.Package{
-			Option: 1,
-			Data:   blockchain.SerializeTX(tx),
-		}
-		network.Send(NODEADDR, &packge)
+	miner := blockchain.NewUser()
+	err := blockchain.NewChain(DBNAME, miner.Address())
+	if err != nil {
+		return
 	}
+
+	chain := blockchain.LoadChain(DBNAME)
+
+	block := blockchain.NewBlock(miner.Address(), chain.LastHash())
+	tx := blockchain.NewTransaction(miner, chain.LastHash(), "MEgCQQCjalTUWYEOJ8X/9ym0JBUyAlse+AydJI6/ybs7vTq8fmP2qSponXndAM7IOrth6JVDdj+2DHuK86gZxloXRlkRAgMBAAE=", 50)
+	err = block.AddTransaction(chain, tx)
+	if err != nil {
+		return
+	}
+
+	err = block.Accept(chain, miner, make(chan bool))
+	if err != nil {
+		return
+	}
+	chain.AddBlock(block)
+
+	listener := network.Listen("localhost:8080", func(conn network.Conn, pack *network.Package) {
+		if network.Handle(TransactionOption, conn, pack, handleTransaction) {
+			fmt.Println("Transaction handled successfully")
+		} else {
+			fmt.Println("Failed to handle transaction")
+		}
+	})
+
+	if listener == nil {
+		fmt.Println("Failed to start server")
+		return
+	}
+
+	select {}
+}
+
+func handleTransaction(pack *network.Package) string {
+	fmt.Printf("Received transaction with option %d and data: %s\n", pack.Option, pack.Data)
+	return "Transaction processed successfully"
 }
