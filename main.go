5efb1e51da76d91132913dd10b635666b92e6613
tyrandel <sidius.py@gmail.com>
2023-09-14 17:27:40 +0300

    node functions doing

--- main.go
+++ main.go
@@ -12,6 +12,14 @@ import (
 
 const DBNAME = "blockchain.db"
 const NODEADDR = "localhost:8080"
+const (
+	AddBlock = iota + 1
+	AddTx
+	GetBlock
+	GetLastHash
+	GetBalance
+	GetChainSize
+)
 
 func main() {
 	user := blockchain.LoadUser("MIIBOwIBAAJBAKNqVNRZgQ4nxf/3KbQkFTICWx74DJ0kjr/Juzu9Orx+Y/apKmided0Azsg6u2HolUN2P7YMe4rzqBnGWhdGWRECAwEAAQJAWQMoZerDA2Ti00RccQVejjj+TWYr6MTrBMjrteSjQ9xtuoik5cA85JMp3T+UOTokD+tQG3SNFETRi42aSDky4QIhANQcqBNbhywtICGiSxT7TRyoVMK+refwJSouApcMAtqVAiEAxTpEynk1F5/I77HSquJd0VBszPalyJDahs+HxKp74Y0CIQC//ZBEtTwMyGulBflf7HdH0TWncGCI59076Jl/juemYQIgdnlyKU52HiLVyWbAbfZc9Qei09y16a1aF/FCVVkz4WECIQDCov/RUtRytQ4C0MVmmLhjz8i7YzYbbiDWV1fPo7rPww==")
@@ -26,7 +34,7 @@ func main() {
 		chain := blockchain.LoadChain(DBNAME)
 		tx := blockchain.NewTransaction(user, chain.LastHash(), toAddr, uint64(amount))
 		packge := network.Package{
-			Option: 1,
+			Option: AddTx,
 			Data:   blockchain.SerializeTX(tx),
 		}
 		network.Send(NODEADDR, &packge)
